9d3d60a5cd1c7ab50caa92ed184c85cb4cf567f4
Bent J. Lorentzen <bent.lorentzen@nav.no>
2023-03-14 15:17:57 +0100

    Sakshendelse for kafka

---
+++ src/main/kotlin/no/nav/bidrag/transport/sak/Sakshendelse.kt
@@ -0,0 +1,28 @@
+package no.nav.bidrag.transport.sak
+
+import no.nav.bidrag.domain.bool.Ukjent
+import no.nav.bidrag.domain.enums.Rolletype
+import no.nav.bidrag.domain.ident.NavCallId
+import no.nav.bidrag.domain.ident.PersonIdent
+import no.nav.bidrag.domain.ident.ReellMottager
+import no.nav.bidrag.domain.ident.SamhandlerId
+import no.nav.bidrag.domain.string.Saksnummer
+
+data class Sakshendelse(
+    val saksnummer: Saksnummer,
+    val hendelsestype: Sakshendelsestype,
+    val roller: List<Saksrolle> = emptyList(),
+    val sporingId: NavCallId
+)
+
+data class Saksrolle(
+    val ident: PersonIdent? = null,
+    val type: Rolletype,
+    val samhandlerId: SamhandlerId? = null,
+    val reelMottager: ReellMottager? = null,
+    val ukjent: Ukjent = Ukjent(false)
+)
+enum class Sakshendelsestype {
+    ENDRING,
+    OPPRETTELSE
+}
