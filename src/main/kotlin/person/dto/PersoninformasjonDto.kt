9c88806a8c20366c6b147b9e4d26993bc2799b67
Simen Høston <simen.naper.hoston@webstep.no>
2023-03-21 14:48:41 +0100

    Reverterter enkelte endringer på felles DTOer som fører til mange endringer i bidrag-person

--- src/main/kotlin/person/dto/PersoninformasjonDto.kt
+++ src/main/kotlin/person/dto/PersoninformasjonDto.kt
@@ -5,9 +5,9 @@ import io.swagger.v3.oas.annotations.media.Schema
 @Schema(description = "Representerer en person med tilhørende informasjon om navn, fødselsdato, adresse, gradering, språk, dødsdato, dødsbo og tidligere identer")
 data class PersoninformasjonDto(
     val person: PersonDto,
-    val adresse: PersonadresseDto?,
+    val adresse: PersonAdresseDto?,
     val kontonummer: KontonummerDto?,
-    val dodsbo: DødsboDto?,
+    val dødsbo: DødsboDto?,
     val språk: String?,
 
     @Schema(description = "Liste over tidligere identer personen har hatt.")
