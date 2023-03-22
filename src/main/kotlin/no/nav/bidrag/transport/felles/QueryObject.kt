516f279939959317d33ebec842eab00d9fe4a329
bent-lorentzen <54444412+bent-lorentzen@users.noreply.github.com>
2023-03-22 09:26:00 +0100

    Fjernet hard validering av rolleDto og fjernet unødvendig jsonCreator (#7)

--- src/main/kotlin/no/nav/bidrag/transport/felles/QueryObject.kt
+++ src/main/kotlin/no/nav/bidrag/transport/felles/QueryObject.kt
@@ -1,13 +1,33 @@
 package no.nav.bidrag.transport.felles
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.datatype.jsr310.deser.YearMonthDeserializer
 import com.fasterxml.jackson.module.kotlin.readValue
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import org.springframework.util.LinkedMultiValueMap
+import java.time.YearMonth
+import java.time.format.DateTimeFormatter
 
 /**
  * Interface med metode for å konvertere et objekt til en LinkedMultiValueMap
  * En LinkedMultiValueMap kan brukes i UriComponentsBuilder med .queryParams(<linkeMultiValueMap>)
  */
 interface QueryObject {
+
+    private val objectMapper: ObjectMapper
+        get() = ObjectMapper()
+            .registerKotlinModule()
+            .registerModule(
+                JavaTimeModule()
+                    .addDeserializer(
+                        YearMonth::class.java,
+                        YearMonthDeserializer(DateTimeFormatter.ofPattern("u-MM")) // Denne trengs for å parse år over 9999 riktig.
+                    )
+            )
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+
     fun toQueryParams(): LinkedMultiValueMap<String, String> {
         val writeValueAsString = objectMapper.writeValueAsString(this)
         val readValue: LinkedHashMap<String, Any?> = objectMapper.readValue(writeValueAsString)
