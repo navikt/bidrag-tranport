8a305cf88fea7832c504a27db298426a8a4f7ca8
Simen Høston <simen.naper.hoston@webstep.no>
2023-03-24 10:37:13 +0100

    Endrer navn til PersonDetaljerDto for å reflektere endepunktet bedre

--- src/main/kotlin/person/dto/PersonDetaljerDto.kt
+++ src/main/kotlin/person/dto/PersonDetaljerDto.kt
@@ -3,7 +3,7 @@ package person.dto
 import io.swagger.v3.oas.annotations.media.Schema
 
 @Schema(description = "Representerer en person med tilhørende informasjon om navn, fødselsdato, adresse, gradering, språk, dødsdato, dødsbo og tidligere identer")
-data class PersoninformasjonDto(
+data class PersonDetaljerDto(
     val person: PersonDto,
     val adresse: PersonAdresseDto?,
     val kontonummer: KontonummerDto?,
