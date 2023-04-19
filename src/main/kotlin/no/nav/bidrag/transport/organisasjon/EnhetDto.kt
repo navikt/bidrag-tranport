6f9d0bad49fe38ed87f1d54a4159a2be4f6629b9
Bent Lorentzen <bent.lorentzen@nav.no>
2023-04-19 13:45:51 +0200

    Oppdatert

--- src/main/kotlin/no/nav/bidrag/transport/organisasjon/EnhetDto.kt
+++ src/main/kotlin/no/nav/bidrag/transport/organisasjon/EnhetDto.kt
@@ -1,6 +1,5 @@
 package no.nav.bidrag.transport.organisasjon
 
-import io.swagger.v3.oas.annotations.media.Schema
 import no.nav.bidrag.domain.enums.Enhetsstatus
 import no.nav.bidrag.domain.string.Adresselinje1
 import no.nav.bidrag.domain.string.Adresselinje2
@@ -12,7 +11,6 @@ import no.nav.bidrag.domain.string.Postnummer
 import no.nav.bidrag.domain.string.Poststed
 import no.nav.bidrag.domain.string.Telefonnummer
 
-@Schema(description = "Metadata for en enhet")
 data class EnhetDto(
     val nummer: Enhetsnummer,
     val navn: Enhetsnavn? = null,
@@ -23,23 +21,40 @@ data class EnhetDto(
     val status: Enhetsstatus = Enhetsstatus.AKTIV
 )
 
-@Schema(description = "Metadata for en enhet postadresse")
 data class EnhetKontaktinfoDto(
     val nummer: Enhetsnummer,
-    val navn: Enhetsnavn? = Enhetsnavn("NAV Familie- og pensjonsytelser"),
+    val navn: Enhetsnavn? = null,
     @Deprecated("bruk nummer", ReplaceWith("nummer"))
     val enhetIdent: Enhetsnummer = nummer,
     @Deprecated("bruk navn", ReplaceWith("navn"))
     val enhetNavn: Enhetsnavn? = navn,
-    val telefonnummer: Telefonnummer? = Telefonnummer("55553333"),
+    val telefonnummer: Telefonnummer? = null,
     val postadresse: EnhetspostadresseDto? = null
-)
+
+
+) {
+
+    companion object {
+        fun medStandardadresse(enhetsnummer: Enhetsnummer) = EnhetKontaktinfoDto(
+            nummer = enhetsnummer,
+            navn = Enhetsnavn("NAV Familie- og pensjonsytelser"),
+            telefonnummer = Telefonnummer("55553333"),
+            postadresse = EnhetspostadresseDto(
+                postnummer = Postnummer("0603"),
+                adresselinje1 = Adresselinje1("Postboks 6215 Etterstad"),
+                poststed = Poststed("Oslo"),
+                land = Landnavn("Norway"),
+                kommunenr = Kommunenummer("0301")
+            )
+        )
+    }
+}
 
 data class EnhetspostadresseDto(
-    val postnummer: Postnummer? = Postnummer("0603"),
-    val adresselinje1: Adresselinje1? = Adresselinje1("Postboks 6215 Etterstad"),
+    val postnummer: Postnummer? = null,
+    val adresselinje1: Adresselinje1? = null,
     val adresselinje2: Adresselinje2? = null,
-    val poststed: Poststed? = Poststed("Oslo"),
-    val land: Landnavn? = Landnavn("Norway"),
-    val kommunenr: Kommunenummer? = Kommunenummer("0301")
+    val poststed: Poststed? = null,
+    val land: Landnavn? = null,
+    val kommunenr: Kommunenummer? = null
 )
