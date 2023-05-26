bb68e0943365180e4d0c933574bec23f2b67beca
bent-lorentzen <54444412+bent-lorentzen@users.noreply.github.com>
2023-05-26 15:10:13 +0200

    Endret map til list (#25)

--- src/main/kotlin/no/nav/bidrag/transport/tilgang/Sporingsdata.kt
+++ src/main/kotlin/no/nav/bidrag/transport/tilgang/Sporingsdata.kt
@@ -3,11 +3,11 @@ package no.nav.bidrag.transport.tilgang
 import io.swagger.v3.oas.annotations.media.Schema
 
 @Schema(description = "Sporingdata til auditlogging")
-data class Sporingsdata(
+class Sporingsdata(
     @Schema(description = "Fødselsnummer på primærpersonen i oppslaget.")
     val personIdent: String,
     @Schema(description = "Hvor vist personen som gjør oppslaget har tilgang til å gjøre oppslaget.")
     val tilgang: Boolean,
     @Schema(description = """Ekstrainformasjonsfelter. Navn og verdi. Eksempelvis mapOf("saksnummer" to "2302845")""")
-    val ekstrafelter: Map<String, String> = emptyMap()
+    val ekstrafelter: List<Pair<String, String>> = listOf()
 )
