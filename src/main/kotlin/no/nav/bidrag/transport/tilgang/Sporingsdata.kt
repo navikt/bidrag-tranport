f1f9d8e943d63c027a2f51d26c314037d78e0fce
Bent J. Lorentzen <bent.lorentzen@nav.no>
2023-05-26 12:01:51 +0200

    Sporingsdata til audit-logg

---
+++ src/main/kotlin/no/nav/bidrag/transport/tilgang/Sporingsdata.kt
@@ -0,0 +1,13 @@
+package no.nav.bidrag.transport.tilgang
+
+import io.swagger.v3.oas.annotations.media.Schema
+
+@Schema(description = "Sporingdata til auditlogging")
+class Sporingsdata(
+    @Schema(description = "Fødselsnummer på primærpersonen i oppslaget.")
+    val personIdent: String,
+    @Schema(description = "Hvor vist personen som gjør oppslaget har tilgang til å gjøre oppslaget.")
+    val tilgang: Boolean,
+    @Schema(description = """Ekstrainformasjonsfelter. Navn og verdi. Eksempelvis "saksnummer to 2302845""")
+    vararg ekstrafelter: Pair<String, String>
+)
