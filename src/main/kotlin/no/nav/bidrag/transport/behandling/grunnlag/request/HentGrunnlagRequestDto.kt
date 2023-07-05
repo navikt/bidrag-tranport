c4717ff236ef0664a952ff2419c0fa144e94da3b
rinnan17 <magnus.rinnan@nav.no>
2023-07-05 07:47:06 +0200

    Dto'er for arbeidsforhold lagt til for grunnlag

---
+++ src/main/kotlin/no/nav/bidrag/transport/behandling/grunnlag/request/HentGrunnlagRequestDto.kt
@@ -0,0 +1,13 @@
+package no.nav.bidrag.transport.behandling.grunnlag.request
+
+import io.swagger.v3.oas.annotations.media.Schema
+import jakarta.validation.Valid
+import jakarta.validation.constraints.NotEmpty
+
+// Request for å hente grunmnlag direkte fra bidrag-grunnlag uten å gå via grunnlagspakke og lagring i bidrag-grunnlag
+data class HentGrunnlagRequestDto(
+  @Schema(description = "Liste over hvilke typer grunnlag som skal hentes inn. På nivået under er personId og perioder angitt")
+  @field:Valid
+  @field:NotEmpty(message = "Listen kan ikke være null eller tom.")
+  val grunnlagRequestDtoListe: List<GrunnlagRequestDto>
+)
