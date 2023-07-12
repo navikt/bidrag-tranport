8fb6b4c323cea09193d8c4678d20e46e41b5eb2e
Magnus Rinnan <44389619+rinnan17@users.noreply.github.com>
2023-07-12 08:11:59 +0200

    Retter skrivefeil i package (#33)

--- src/main/kotlin/no/nav/bidrag/transport/behandling/grunnlag/response/OppdaterGrunnlagspakkeDto.kt
+++ src/main/kotlin/no/nav/bidrag/transport/behandling/grunnlag/response/OppdaterGrunnlagspakkeDto.kt
@@ -1,4 +1,4 @@
-package no.nav.bidrag.transport.behandling.grunnlag.reponse
+package no.nav.bidrag.transport.behandling.grunnlag.response
 
 import io.swagger.v3.oas.annotations.media.Schema
 import no.nav.bidrag.domain.enums.GrunnlagRequestType
--- src/main/kotlin/no/nav/bidrag/transport/behandling/stonad/response/StonadDto.kt
+++ src/main/kotlin/no/nav/bidrag/transport/behandling/stonad/response/StonadDto.kt
@@ -1,4 +1,4 @@
-package no.nav.bidrag.transport.behandling.stonad.reponse
+package no.nav.bidrag.transport.behandling.stonad.response
 
 import io.swagger.v3.oas.annotations.media.Schema
 import no.nav.bidrag.domain.enums.Innkreving
