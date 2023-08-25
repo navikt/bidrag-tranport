4159af77b5daed9a82941251fa69629c4c286d57
Magnus Rinnan <44389619+rinnan17@users.noreply.github.com>
2023-08-25 14:51:01 +0200

    Slettet Fellesklasser.kt (#43)

--- src/main/kotlin/no/nav/bidrag/transport/beregning/barnebidrag/core/request/BeregnBarnebidragGrunnlagCore.kt
+++ src/main/kotlin/no/nav/bidrag/transport/beregning/barnebidrag/core/request/BeregnBarnebidragGrunnlagCore.kt
@@ -7,67 +7,67 @@ import java.time.LocalDate
 
 // Grunnlag
 data class BeregnBarnebidragGrunnlagCore(
-  val beregnDatoFra: LocalDate,
-  val beregnDatoTil: LocalDate,
-  val bidragsevnePeriodeListe: List<BidragsevnePeriodeCore>,
-  val bPsAndelUnderholdskostnadPeriodeListe: List<BPsAndelUnderholdskostnadPeriodeCore>,
-  val samvaersfradragPeriodeListe: List<SamvaersfradragPeriodeCore>,
-  val deltBostedPeriodeListe: List<DeltBostedPeriodeCore>,
-  val barnetilleggBPPeriodeListe: List<BarnetilleggPeriodeCore>,
-  val barnetilleggBMPeriodeListe: List<BarnetilleggPeriodeCore>,
-  val barnetilleggForsvaretPeriodeListe: List<BarnetilleggForsvaretPeriodeCore>,
-  val andreLopendeBidragPeriodeListe: List<AndreLopendeBidragPeriodeCore>,
-  val sjablonPeriodeListe: List<SjablonPeriodeCore>
+    val beregnDatoFra: LocalDate,
+    val beregnDatoTil: LocalDate,
+    val bidragsevnePeriodeListe: List<BidragsevnePeriodeCore>,
+    val bPsAndelUnderholdskostnadPeriodeListe: List<BPsAndelUnderholdskostnadPeriodeCore>,
+    val samvaersfradragPeriodeListe: List<SamvaersfradragPeriodeCore>,
+    val deltBostedPeriodeListe: List<DeltBostedPeriodeCore>,
+    val barnetilleggBPPeriodeListe: List<BarnetilleggPeriodeCore>,
+    val barnetilleggBMPeriodeListe: List<BarnetilleggPeriodeCore>,
+    val barnetilleggForsvaretPeriodeListe: List<BarnetilleggForsvaretPeriodeCore>,
+    val andreLopendeBidragPeriodeListe: List<AndreLopendeBidragPeriodeCore>,
+    val sjablonPeriodeListe: List<SjablonPeriodeCore>
 )
 
 data class BidragsevnePeriodeCore(
-  val referanse: String,
-  val periode: PeriodeCore,
-  val belop: BigDecimal,
-  val tjuefemProsentInntekt: BigDecimal
+    val referanse: String,
+    val periode: PeriodeCore,
+    val belop: BigDecimal,
+    val tjuefemProsentInntekt: BigDecimal
 )
 
 data class BPsAndelUnderholdskostnadPeriodeCore(
-  val referanse: String,
-  val soknadsbarnPersonId: Int,
-  val periode: PeriodeCore,
-  val andelProsent: BigDecimal,
-  val andelBelop: BigDecimal,
-  val barnetErSelvforsorget: Boolean
+    val referanse: String,
+    val soknadsbarnPersonId: Int,
+    val periode: PeriodeCore,
+    val andelProsent: BigDecimal,
+    val andelBelop: BigDecimal,
+    val barnetErSelvforsorget: Boolean
 )
 
 data class SamvaersfradragPeriodeCore(
-  val referanse: String,
-  val soknadsbarnPersonId: Int,
-  val periode: PeriodeCore,
-  val belop: BigDecimal
+    val referanse: String,
+    val soknadsbarnPersonId: Int,
+    val periode: PeriodeCore,
+    val belop: BigDecimal
 )
 
 data class DeltBostedPeriodeCore(
-  val referanse: String,
-  val soknadsbarnPersonId: Int,
-  val periode: PeriodeCore,
-  val deltBostedIPeriode: Boolean
+    val referanse: String,
+    val soknadsbarnPersonId: Int,
+    val periode: PeriodeCore,
+    val deltBostedIPeriode: Boolean
 )
 
 data class BarnetilleggPeriodeCore(
-  val referanse: String,
-  val soknadsbarnPersonId: Int,
-  val periode: PeriodeCore,
-  val belop: BigDecimal,
-  val skattProsent: BigDecimal
+    val referanse: String,
+    val soknadsbarnPersonId: Int,
+    val periode: PeriodeCore,
+    val belop: BigDecimal,
+    val skattProsent: BigDecimal
 )
 
 data class BarnetilleggForsvaretPeriodeCore(
-  val referanse: String,
-  val periode: PeriodeCore,
-  val barnetilleggForsvaretIPeriode: Boolean
+    val referanse: String,
+    val periode: PeriodeCore,
+    val barnetilleggForsvaretIPeriode: Boolean
 )
 
 data class AndreLopendeBidragPeriodeCore(
-  val referanse: String,
-  val periode: PeriodeCore,
-  val barnPersonId: Int,
-  val bidragBelop: BigDecimal,
-  val samvaersfradragBelop: BigDecimal
+    val referanse: String,
+    val periode: PeriodeCore,
+    val barnPersonId: Int,
+    val bidragBelop: BigDecimal,
+    val samvaersfradragBelop: BigDecimal
 )
--- src/main/kotlin/no/nav/bidrag/transport/beregning/barnebidrag/rest/request/BeregnGrunnlag.kt
+++ src/main/kotlin/no/nav/bidrag/transport/beregning/barnebidrag/rest/request/BeregnGrunnlag.kt
@@ -6,27 +6,27 @@ import java.time.LocalDate
 
 @Schema(description = "Grunnlaget for en beregning av barnebidrag")
 data class BeregnGrunnlag(
-  @Schema(description = "Beregn fra-dato") val beregnDatoFra: LocalDate? = null,
-  @Schema(description = "Beregn til-dato") val beregnDatoTil: LocalDate? = null,
-  @Schema(description = "Periodisert liste over grunnlagselementer") val grunnlagListe: List<Grunnlag>? = null
+    @Schema(description = "Beregn fra-dato") val beregnDatoFra: LocalDate? = null,
+    @Schema(description = "Beregn til-dato") val beregnDatoTil: LocalDate? = null,
+    @Schema(description = "Periodisert liste over grunnlagselementer") val grunnlagListe: List<Grunnlag>? = null
 )
 
 @Schema(description = "Grunnlag")
 data class Grunnlag(
-  @Schema(description = "Referanse") val referanse: String? = null,
-  @Schema(description = "Type") val type: String? = null,
-  @Schema(description = "Innhold") val innhold: JsonNode? = null
+    @Schema(description = "Referanse") val referanse: String? = null,
+    @Schema(description = "Type") val type: String? = null,
+    @Schema(description = "Innhold") val innhold: JsonNode? = null
 )
 
 @Schema(description = "Grunnlaget for en beregning av forholdsmessig fordeling")
 data class BeregnGrunnlagFF(
-  @Schema(description = "Beregn fra-dato") val beregnDatoFra: LocalDate? = null,
-  @Schema(description = "Beregn til-dato") val beregnDatoTil: LocalDate? = null,
-  @Schema(description = "Periodisert liste over grunnlagselementer") val grunnlagListe: List<GrunnlagFF>? = null
+    @Schema(description = "Beregn fra-dato") val beregnDatoFra: LocalDate? = null,
+    @Schema(description = "Beregn til-dato") val beregnDatoTil: LocalDate? = null,
+    @Schema(description = "Periodisert liste over grunnlagselementer") val grunnlagListe: List<GrunnlagFF>? = null
 )
 
 @Schema(description = "Grunnlag")
 data class GrunnlagFF(
-  @Schema(description = "Type") val type: String? = null,
-  @Schema(description = "Innhold") val innhold: JsonNode? = null
+    @Schema(description = "Type") val type: String? = null,
+    @Schema(description = "Innhold") val innhold: JsonNode? = null
 )
