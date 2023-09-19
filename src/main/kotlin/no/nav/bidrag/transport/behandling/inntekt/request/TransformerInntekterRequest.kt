6f501ab93b5b66a6c2c35e2d3e6dc4526ab9492c
Jan Kjetil Skår <43172034+s148719@users.noreply.github.com>
2023-09-19 15:56:06 +0200

    bd-1716: Forenkle bidrag-inntekt-api, ainntekt i transformer-inntekte… (#53)
    * bd-1716: Forenkle bidrag-inntekt-api, ainntekt i transformer-inntekter-request
    
    * bd-176: Fjerne kontantstotteListe fra TransformerInntekterRequest ettersom denne informasjonen ikke brukes til noe i bidrag-inntekt
    
    * bd-1716: Forenkle TransformerInntekterRequest - Fjerne ubstListe og refaktorisere overgansstonadsliste.
    
    * bd-1716: Konsistens

--- src/main/kotlin/no/nav/bidrag/transport/behandling/inntekt/request/TransformerInntekterRequest.kt
+++ src/main/kotlin/no/nav/bidrag/transport/behandling/inntekt/request/TransformerInntekterRequest.kt
@@ -1,11 +1,9 @@
 package no.nav.bidrag.transport.behandling.inntekt.request
 
 import io.swagger.v3.oas.annotations.media.Schema
-import no.nav.bidrag.transport.behandling.grunnlag.response.AinntektDto
-import no.nav.bidrag.transport.behandling.grunnlag.response.KontantstotteDto
-import no.nav.bidrag.transport.behandling.grunnlag.response.OvergangsstonadDto
 import no.nav.bidrag.transport.behandling.grunnlag.response.SkattegrunnlagspostDto
-import no.nav.bidrag.transport.behandling.grunnlag.response.UtvidetBarnetrygdOgSmaabarnstilleggDto
+import java.math.BigDecimal
+import java.time.LocalDate
 
 // TODO Legge til swagger-doc, default-verdier
 // TODO Legge til evt. manuelle inntekter
@@ -13,19 +11,13 @@ import no.nav.bidrag.transport.behandling.grunnlag.response.UtvidetBarnetrygdOgS
 
 data class TransformerInntekterRequest(
     @Schema(description = "Periodisert liste over inntekter fra Ainntekt")
-    val ainntektListe: List<AinntektDto> = emptyList(),
+    val ainntektsposter: List<Ainntektspost> = emptyList(),
 
     @Schema(description = "Periodisert liste over inntekter fra Sigrun")
-    val skattegrunnlagListe: List<SkattegrunnlagForLigningsår> = emptyList(),
-
-    @Schema(description = "Periodisert liste over utvidet barnetrygd og småbarnstillegg")
-    val ubstListe: List<UtvidetBarnetrygdOgSmaabarnstilleggDto> = emptyList(),
-
-    @Schema(description = "Periodisert liste over kontantstøtte")
-    val kontantstotteListe: List<KontantstotteDto> = emptyList(),
+    val skattegrunnlagsliste: List<SkattegrunnlagForLigningsår> = emptyList(),
 
     @Schema(description = "Periodisert liste over overgangsstønad")
-    val overgangsstonadListe: List<OvergangsstonadDto> = emptyList()
+    val overgangsstonadsliste: List<Overgangsstonad> = emptyList()
 )
 
 data class SkattegrunnlagForLigningsår(
@@ -35,5 +27,34 @@ data class SkattegrunnlagForLigningsår(
 
     @Schema(description = "Poster med skattegrunnlag")
     val skattegrunnlagsposter: List<SkattegrunnlagspostDto>
+)
 
+data class Ainntektspost(
+
+    @Schema(description = "Perioden innteksposten er utbetalt YYYYMM")
+    val utbetalingsperiode: String?,
+
+    @Schema(description = "Fra-dato for opptjening")
+    val opptjeningsperiodeFra: LocalDate?,
+
+    @Schema(description = "Til-dato for opptjening")
+    val opptjeningsperiodeTil: LocalDate?,
+
+    @Schema(description = "Beskrivelse av inntekt")
+    val beskrivelse: String?,
+
+    @Schema(description = "Belop")
+    val belop: BigDecimal
+)
+
+data class Overgangsstonad(
+
+    @Schema(description = "Periode fra-dato")
+    val periodeFra: LocalDate,
+
+    @Schema(description = "Periode til-dato")
+    val periodeTil: LocalDate?,
+
+    @Schema(description = "Beløp overgangsstønad")
+    val belop: Int
 )
