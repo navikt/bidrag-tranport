a8057e84c273d9756e60b5b50c53891e1ace26a1
Magnus Rinnan <44389619+rinnan17@users.noreply.github.com>
2023-09-25 15:19:10 +0200

    Endret sivilstand, legger med noe metadata for å kunne periodisere bedre i bidrag-grunnlag (#59)

--- src/main/kotlin/no/nav/bidrag/transport/person/SivilstandDto.kt
+++ src/main/kotlin/no/nav/bidrag/transport/person/SivilstandDto.kt
@@ -5,15 +5,18 @@ import io.swagger.v3.oas.annotations.media.Schema
 import no.nav.bidrag.domain.enums.Sivilstandstype
 import no.nav.bidrag.domain.tid.Bekreftelsesdato
 import no.nav.bidrag.domain.tid.FomDato
+import java.time.LocalDateTime
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-data class Sivilstandshistorikk(
+data class SivilstandshistorikkDto(
     @Schema(description = "Liste over alle hentede forekomster av sivilstand fra bidrag-person")
-    val sivilstand: List<Sivilstand>
+    val sivilstandDto: List<SivilstandDto>
 )
 
-data class Sivilstand(
+data class SivilstandDto(
     val type: Sivilstandstype?,
     val gyldigFraOgMed: FomDato?,
-    val bekreftelsesdato: Bekreftelsesdato?
+    val bekreftelsesdato: Bekreftelsesdato?,
+    val master: String?,
+    val registrert: LocalDateTime?,
 )
