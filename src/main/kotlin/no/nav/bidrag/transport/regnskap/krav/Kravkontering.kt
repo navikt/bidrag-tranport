5f5f1a4dd652c7f65fc98fadeddf23cd8e83f6be
Magnus Rinnan <44389619+rinnan17@users.noreply.github.com>
2023-10-16 15:26:13 +0200

    Lagt til kontantstøtte og utvidet barnetrygd i request til bidrag-inntekt (#65)

--- src/main/kotlin/no/nav/bidrag/transport/regnskap/krav/Kravkontering.kt
+++ src/main/kotlin/no/nav/bidrag/transport/regnskap/krav/Kravkontering.kt
@@ -8,32 +8,32 @@ import java.math.BigDecimal
 
 @Schema(
     description = "\t\n" +
-            "En kontering angir hvor mye som skal betales av skyldner til mottaker på vegne av kravhaver.\n" +
-            "\n" +
-            "Konteringen kan unikt identifiseres med kombinasjonen transaksjonskode, delytelsesId og periode. Det forutsettes at delytelsesid'n er unik også på tvers av fagsystemid'er.\n" +
-            "\n" +
-            "Personidenter for gjelderIdent, kravhaverIdent, mottakerIdent og skyldnerIdent angis med enten FNR eller DNR. (Håndtering av BNR og NPID er uavklart.) Aktoernummer kan benyttes i kravhaverIdent, mottakerIdent og skyldnerIdent. Aktoernummere er elleve siffer og starter med enten 8 eller 9.\n" +
-            "\n" +
-            "I testmiljøene må Tenor-identer støttes i stedet for FNR/DNR. Disse identene har 8 eller 9 i tredje siffer."
+        "En kontering angir hvor mye som skal betales av skyldner til mottaker på vegne av kravhaver.\n" +
+        "\n" +
+        "Konteringen kan unikt identifiseres med kombinasjonen transaksjonskode, delytelsesId og periode. Det forutsettes at delytelsesid'n er unik også på tvers av fagsystemid'er.\n" +
+        "\n" +
+        "Personidenter for gjelderIdent, kravhaverIdent, mottakerIdent og skyldnerIdent angis med enten FNR eller DNR. (Håndtering av BNR og NPID er uavklart.) Aktoernummer kan benyttes i kravhaverIdent, mottakerIdent og skyldnerIdent. Aktoernummere er elleve siffer og starter med enten 8 eller 9.\n" +
+        "\n" +
+        "I testmiljøene må Tenor-identer støttes i stedet for FNR/DNR. Disse identene har 8 eller 9 i tredje siffer."
 )
 data class Kravkontering(
 
     @field:Schema(
         description = "Type transaksjon.\n\n" +
-                "Gyldige transaksjonskoder er:\n" +
-                "| Kode  | Korreksjonskode | Beskrivelse                                |\n" +
-                "|-------|-----------------|--------------------------------------------|\n" +
-                "| A1    | A3              | Bidragsforskudd                            |\n" +
-                "| B1    | B3              | Underholdsbidrag (m/u tilleggsbidrag)      |\n" +
-                "| D1    | D3              | 18årsbidrag                                |\n" +
-                "| E1    | E3              | Bidrag til særlige utgifter (særtilskudd)  |\n" +
-                "| F1    | F3              | Ekrefellebidrag                            |\n" +
-                "| G1    | G3              | Gebyr                                      |\n" +
-                "| H1    | H3              | Tilbakekreving                             |\n" +
-                "| I1    |                 | Motregning                                 |\n" +
-                "| K1    |                 | Ettergivelse                               |\n" +
-                "| K2    |                 | Direkte oppgjør (innbetalt beløp)          |\n" +
-                "| K3    |                 | Tilbakekreving ettergivelse                |\n",
+            "Gyldige transaksjonskoder er:\n" +
+            "| Kode  | Korreksjonskode | Beskrivelse                                |\n" +
+            "|-------|-----------------|--------------------------------------------|\n" +
+            "| A1    | A3              | Bidragsforskudd                            |\n" +
+            "| B1    | B3              | Underholdsbidrag (m/u tilleggsbidrag)      |\n" +
+            "| D1    | D3              | 18årsbidrag                                |\n" +
+            "| E1    | E3              | Bidrag til særlige utgifter (særtilskudd)  |\n" +
+            "| F1    | F3              | Ekrefellebidrag                            |\n" +
+            "| G1    | G3              | Gebyr                                      |\n" +
+            "| H1    | H3              | Tilbakekreving                             |\n" +
+            "| I1    |                 | Motregning                                 |\n" +
+            "| K1    |                 | Ettergivelse                               |\n" +
+            "| K2    |                 | Direkte oppgjør (innbetalt beløp)          |\n" +
+            "| K3    |                 | Tilbakekreving ettergivelse                |\n",
         example = "B1",
         required = true
     ) val transaksjonskode: Transaksjonskode,
@@ -46,12 +46,12 @@ data class Kravkontering(
 
     @field:Schema(
         description = "Angirtypen behandling som har ført til konteringen.\n" +
-                "| Kode | Beskrivelse                                                                |\n" +
-                "|------|----------------------------------------------------------------------------|\n" +
-                "| IN   | Sendes første måned i et indeksreguleringsvedtak. Etter dette benyttes EN. |\n" +
-                "| FABM | Benyttes for gebyr som gjelder BM.                                         |\n" +
-                "| FABP | Benyttes for gebyr som gjelder BP.                                         |\n" +
-                "| EN   | Alle andre typer endringer. Også førstegangsvedtak.                        |\n",
+            "| Kode | Beskrivelse                                                                |\n" +
+            "|------|----------------------------------------------------------------------------|\n" +
+            "| IN   | Sendes første måned i et indeksreguleringsvedtak. Etter dette benyttes EN. |\n" +
+            "| FABM | Benyttes for gebyr som gjelder BM.                                         |\n" +
+            "| FABP | Benyttes for gebyr som gjelder BP.                                         |\n" +
+            "| EN   | Alle andre typer endringer. Også førstegangsvedtak.                        |\n",
         example = "EN",
         required = true
     ) val soknadType: Søknadstype,
@@ -64,21 +64,21 @@ data class Kravkontering(
 
     @field:Schema(
         description = "Personident (FNR/DNR) eller aktoernummer (TSS-ident/samhandler) til kravhaver." +
-                "\n\nKravhaver angis ikke for gebyr.",
+            "\n\nKravhaver angis ikke for gebyr.",
         example = "14871298182",
         required = false
     ) val kravhaverIdent: String?,
 
     @field:Schema(
         description = "Personident (FNR/DNR) eller aktoernummer (TSS-ident/samhandler) til mottaker av kravet." +
-                "\n\nFor gebyr settes mottakerIdent til NAVs aktoernummer 80000345435.",
+            "\n\nFor gebyr settes mottakerIdent til NAVs aktoernummer 80000345435.",
         example = "15878598161",
         required = true
     ) val mottakerIdent: String,
 
     @field:Schema(
         description = "Personident (FNR/DNR) eller aktoernummer (TSS-ident/samhandler) til skyldner. For Bidrag er dette BP i saken." +
-                "\n\nFor forskudd settes skyldnerIdent til NAVs aktoernummer 80000345435.",
+            "\n\nFor forskudd settes skyldnerIdent til NAVs aktoernummer 80000345435.",
         example = "28848596401",
         required = true
     ) val skyldnerIdent: String,
@@ -111,8 +111,8 @@ data class Kravkontering(
 
     @field:Schema(
         description = "Datoen kravet/konteringen gjøres klart for overføring. " +
-                "For direkteoverførte online-vedtak blir datoen sannsynligvis det samme som vedtaksdato. " +
-                "For påløp blir datoen satt til dagen påløpet genereres.",
+            "For direkteoverførte online-vedtak blir datoen sannsynligvis det samme som vedtaksdato. " +
+            "For påløp blir datoen satt til dagen påløpet genereres.",
         example = "2022-03-18",
         required = true
     ) val kjoredato: String,
@@ -131,7 +131,7 @@ data class Kravkontering(
 
     @field:Schema(
         description = "Felt hvor utlandsavdelingen legger inn referansenummer (ffu-ref). " +
-                "Dette er et fritekstfelt som kan inneholde spesialtegn.",
+            "Dette er et fritekstfelt som kan inneholde spesialtegn.",
         example = "VII W → 450 → 40 /11",
         required = false
     ) val tekst: String?,
@@ -144,8 +144,8 @@ data class Kravkontering(
 
     @field:Schema(
         description = "Unik referanse til oppdragsperioden i vedtaket angitt som String. " +
-                "I bidragssaken kan en oppdragsperiode strekke over flere måneder, og samme referanse blir da benyttet for alle månedene. " +
-                "Samme referanse kan ikke benyttes to ganger for samme transaksjonskode i samme måned.",
+            "I bidragssaken kan en oppdragsperiode strekke over flere måneder, og samme referanse blir da benyttet for alle månedene. " +
+            "Samme referanse kan ikke benyttes to ganger for samme transaksjonskode i samme måned.",
         example = "123456789",
         required = true
     ) val delytelsesId: String
