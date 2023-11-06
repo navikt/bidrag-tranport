847815b0f99f81797437c8f94c0ee37f20c56e0b
Lars Otto Haugen <45556722+LarsOttoHaugen@users.noreply.github.com>
2023-11-06 14:31:53 +0100

    Endret periodetype til ÅrMånedPeriode på grunnlag i forskuddsberegning (#93)

--- src/main/kotlin/no/nav/bidrag/transport/behandling/beregning/felles/grunnlag/InntektRapporteringPeriode.kt
+++ src/main/kotlin/no/nav/bidrag/transport/behandling/beregning/felles/grunnlag/InntektRapporteringPeriode.kt
@@ -2,12 +2,12 @@ package no.nav.bidrag.transport.behandling.beregning.felles.grunnlag
 
 import io.swagger.v3.oas.annotations.media.Schema
 import no.nav.bidrag.domene.enums.InntektRapportering
-import no.nav.bidrag.domene.tid.Datoperiode
+import no.nav.bidrag.domene.tid.ÅrMånedsperiode
 import java.math.BigDecimal
 
 @Schema(description = "Inntekt for person")
 data class InntektRapporteringPeriode(
-    @Schema(description = "Periode") val periode: Datoperiode,
+    @Schema(description = "Periode") val periode: ÅrMånedsperiode,
     @Schema(description = "Type inntektrapportering") val inntektRapportering: InntektRapportering,
     @Schema(description = "Inntekt beløp") val beløp: BigDecimal,
     @Schema(description = "Er registrert manuelt?") val manueltRegistrert: Boolean,
