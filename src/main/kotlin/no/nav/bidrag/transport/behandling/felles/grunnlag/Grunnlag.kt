8a943198e1cbc604cd67f0b17c71c70a02553852
LarsOttoHaugen <lars.otto.haugen@nav.no>
2023-11-06 14:28:47 +0100

    Endret periodetype til ÅrMånedPeriode på grunnlag i forskuddsberegning

--- src/main/kotlin/no/nav/bidrag/transport/behandling/felles/grunnlag/Grunnlag.kt
+++ src/main/kotlin/no/nav/bidrag/transport/behandling/felles/grunnlag/Grunnlag.kt
@@ -4,8 +4,8 @@ import io.swagger.v3.oas.annotations.media.Schema
 import no.nav.bidrag.domene.enums.Bostatuskode
 import no.nav.bidrag.domene.enums.SivilstandskodeBeregning
 import no.nav.bidrag.domene.ident.Personident
-import no.nav.bidrag.domene.tid.Datoperiode
 import no.nav.bidrag.domene.tid.Fødselsdato
+import no.nav.bidrag.domene.tid.ÅrMånedsperiode
 import java.time.LocalDate
 
 @Schema(description = "Informasjon om en person som er inkludert i vedtaket")
@@ -17,13 +17,13 @@ data class Person(
 
 @Schema(description = "Bostatus for person")
 data class BostatusPeriode(
-    @Schema(description = "Periode") val periode: Datoperiode,
+    @Schema(description = "Periode") val periode: ÅrMånedsperiode,
     @Schema(description = "Bostatus") val bostatus: Bostatuskode,
     @Schema(description = "Er registrert manuelt?") val manueltRegistrert: Boolean,
 )
 
 @Schema(description = "Sivilstand for person")
 data class SivilstandPeriode(
-    @Schema(description = "Periode") val periode: Datoperiode,
+    @Schema(description = "Periode") val periode: ÅrMånedsperiode,
     @Schema(description = "Sivilstand") val sivilstand: SivilstandskodeBeregning,
 )
