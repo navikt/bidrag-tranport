329044d80e51a3527d1793046b2f487c14dd88be
Magnus Rinnan <44389619+rinnan17@users.noreply.github.com>
2023-11-08 11:58:02 +0100

    Lagt til OpprettVedtakResponseDto for å svare med referansen for engangsbeløp ved opprettelse av vedtak (#99)

---
+++ src/main/kotlin/no/nav/bidrag/transport/behandling/vedtak/response/OpprettVedtakResponseDto.kt
@@ -0,0 +1,32 @@
+package no.nav.bidrag.transport.behandling.vedtak.response
+
+import com.fasterxml.jackson.databind.JsonNode
+import io.swagger.v3.oas.annotations.media.Schema
+import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.NotBlank
+import no.nav.bidrag.domene.enums.BehandlingsrefKilde
+import no.nav.bidrag.domene.enums.Beslutningstype
+import no.nav.bidrag.domene.enums.Engangsbeløptype
+import no.nav.bidrag.domene.enums.Grunnlagstype
+import no.nav.bidrag.domene.enums.Innkrevingstype
+import no.nav.bidrag.domene.enums.Stønadstype
+import no.nav.bidrag.domene.enums.Vedtakskilde
+import no.nav.bidrag.domene.enums.Vedtakstype
+import no.nav.bidrag.domene.ident.Personident
+import no.nav.bidrag.domene.streng.Enhetsnummer
+import no.nav.bidrag.domene.streng.Saksnummer
+import no.nav.bidrag.domene.tid.ÅrMånedsperiode
+import java.math.BigDecimal
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+@Schema
+data class OpprettVedtakResponseDto(
+
+    @Schema(description = "Unik id generert for vedtak")
+    val vedtaksid: Int,
+
+    @Schema(description = "Liste over alle referansen for engangsbeløp som inngår i vedtaket. Ligger i samme rekkefølge som i requesten")
+    val engangsbeløpReferanseListe: List<String>,
+
+)
