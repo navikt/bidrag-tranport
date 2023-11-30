b66e902af07743fb95e4a61c60baa312438d948e
Lars Otto Haugen <45556722+LarsOttoHaugen@users.noreply.github.com>
2023-11-30 13:39:57 +0100

    Renamet klasse fra SivilstandDto til SivilstandPersonDto for å unngå duplikat klassenavn (#124)

--- src/main/kotlin/no/nav/bidrag/transport/person/SivilstandPersonDto.kt
+++ src/main/kotlin/no/nav/bidrag/transport/person/SivilstandPersonDto.kt
@@ -1,21 +1,18 @@
 package no.nav.bidrag.transport.person
 
 import com.fasterxml.jackson.annotation.JsonInclude
-import io.swagger.annotations.ApiModel
 import io.swagger.v3.oas.annotations.media.Schema
 import no.nav.bidrag.domene.enums.person.SivilstandskodePDL
-import no.nav.bidrag.transport.behandling.grunnlag.response.SivilstandDto
 import java.time.LocalDate
 import java.time.LocalDateTime
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 data class SivilstandshistorikkDto(
     @Schema(description = "Liste over alle hentede forekomster av sivilstand fra bidrag-person")
-    val sivilstandDto: List<SivilstandDto>,
+    val sivilstandDto: List<SivilstandPersonDto>,
 )
 
-@ApiModel("SivilstandDtoPerson")
-data class SivilstandDto(
+data class SivilstandPersonDto(
     val type: SivilstandskodePDL?,
     val gyldigFraOgMed: LocalDate?,
     val bekreftelsesdato: LocalDate?,
