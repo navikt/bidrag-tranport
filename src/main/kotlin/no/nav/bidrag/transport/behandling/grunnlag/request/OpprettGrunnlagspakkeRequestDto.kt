8d33debf8179c0d521faf7e81c166e7ecf40ce0e
Ugur Alpay Cenar <cenar93@gmail.com>
2023-12-01 15:01:18 +0100

    Opprett grunnlagspakke opprettetAv felt til nullable (#126)

--- src/main/kotlin/no/nav/bidrag/transport/behandling/grunnlag/request/OpprettGrunnlagspakkeRequestDto.kt
+++ src/main/kotlin/no/nav/bidrag/transport/behandling/grunnlag/request/OpprettGrunnlagspakkeRequestDto.kt
@@ -1,7 +1,6 @@
 package no.nav.bidrag.transport.behandling.grunnlag.request
 
 import io.swagger.v3.oas.annotations.media.Schema
-import jakarta.validation.constraints.NotBlank
 import no.nav.bidrag.domene.enums.vedtak.Formål
 
 @Schema(description = "Request for å opprette ny grunnlagspakke, uten annet innhold")
@@ -10,7 +9,13 @@ data class OpprettGrunnlagspakkeRequestDto(
     @Schema(description = "Til hvilket formål skal grunnlagspakken benyttes. BIDRAG, FORSKUDD eller SAERTILSKUDD")
     val formaal: Formål,
 
-    @Schema(description = "opprettet av")
-    @field:NotBlank(message = "Kan ikke være null eller blank.")
-    val opprettetAv: String,
+    @Schema(
+        description = """
+Id på hvem som oppretter grunnlagspakken. Kan være NAV-ident til saksbehandler eller Id på batchjobb.
+        
+`opprettetAv` må settes hvis grunnlagspakken opprettes i system kontekst (feks maskin-maskin kommunikasjon). Ellers hentes NAV-ident til sakbehandler fra tokenet
+    """,
+        example = "BATCH_1234/Z99999",
+    )
+    val opprettetAv: String? = null,
 )
