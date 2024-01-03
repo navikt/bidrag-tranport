9f5809f958122068f47aaf804a0bb5db1be87e8e
Lars Otto Haugen <45556722+LarsOttoHaugen@users.noreply.github.com>
2024-01-03 14:43:24 +0100

    Utvidet HentGrunnlagRequestDto (#139)

--- src/main/kotlin/no/nav/bidrag/transport/behandling/grunnlag/request/HentGrunnlagRequestDto.kt
+++ src/main/kotlin/no/nav/bidrag/transport/behandling/grunnlag/request/HentGrunnlagRequestDto.kt
@@ -3,9 +3,14 @@ package no.nav.bidrag.transport.behandling.grunnlag.request
 import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.Valid
 import jakarta.validation.constraints.NotEmpty
+import no.nav.bidrag.domene.enums.vedtak.Formål
 
-// Request for å hente grunmnlag direkte fra bidrag-grunnlag uten å gå via grunnlagspakke og lagring i bidrag-grunnlag
+// Request for å hente grunnlag direkte via eksterne tjenester uten å gå via grunnlagspakke og lagring i bidrag-grunnlag
 data class HentGrunnlagRequestDto(
+
+    @Schema(description = "Formål (BIDRAG, FORSKUDD eller SAERTILSKUDD). Brukes for å hente Ainntekt")
+    val formaal: Formål,
+
     @Schema(description = "Liste over hvilke typer grunnlag som skal hentes inn. På nivået under er personId og perioder angitt")
     @field:Valid
     @field:NotEmpty(message = "Listen kan ikke være null eller tom.")
