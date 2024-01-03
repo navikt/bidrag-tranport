76d66ff7824ac434a2233028c145ce169c9b4ace
LarsOttoHaugen <lars.otto.haugen@nav.no>
2024-01-03 14:13:34 +0100

    Utvidet HentGrunnlagDto

--- src/main/kotlin/no/nav/bidrag/transport/behandling/grunnlag/response/HentGrunnlagDto.kt
+++ src/main/kotlin/no/nav/bidrag/transport/behandling/grunnlag/response/HentGrunnlagDto.kt
@@ -1,5 +1,36 @@
 package no.nav.bidrag.transport.behandling.grunnlag.response
 
+import io.swagger.v3.oas.annotations.media.Schema
+
 data class HentGrunnlagDto(
+
+    @Schema(description = "Periodisert liste over innhentede inntekter fra a-inntekt og underliggende poster")
+    val ainntektListe: List<AinntektDto>,
+
+    @Schema(description = "Periodisert liste over innhentede fra skatt og underliggende poster")
+    val skattegrunnlagListe: List<SkattegrunnlagDto>,
+
+    @Schema(description = "Periodisert liste over innhentet utvidet barnetrygd og småbarnstillegg")
+    val ubstListe: List<UtvidetBarnetrygdOgSmaabarnstilleggDto>,
+
+    @Schema(description = "Periodisert liste over innhentet barnetillegg")
+    val barnetilleggListe: List<BarnetilleggDto>,
+
+    @Schema(description = "Periodisert liste over innhentet kontantstøtte")
+    val kontantstøtteListe: List<KontantstotteDto>,
+
+    @Schema(
+        description = "Liste over alle personer som har bodd sammen med BM/BP i perioden fra virkningstidspunkt og fremover med en liste " +
+            "over hvilke perioder de har delt bolig. Listen inkluderer i tillegg personens egne barn, selv om de ikke har delt bolig med BM/BP",
+    )
+    val husstandsmedlemmerOgEgneBarnListe: List<RelatertPersonDto>,
+
+    @Schema(description = "Periodisert liste over en persons sivilstand")
+    val sivilstandListe: List<SivilstandDto>,
+
+    @Schema(description = "Periodisert liste over innhentet barnetilsyn")
+    val barnetilsynListe: List<BarnetilsynDto>,
+
+    @Schema(description = "Periodisert liste over arbeidsforhold")
     val arbeidsforholdListe: List<ArbeidsforholdDto>,
 )
